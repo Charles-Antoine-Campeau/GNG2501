e7de0895cf5d3b1a136f56629d903b74f6ea390a
Charles-Antoine-Campeau <campeauca@gmail.com>
2021-02-13 13:38:44 -0500

    Implementing onCreate function and creating onClick function.  Minor changes to the menu layout

--- SuivitdeMedicaments/app/src/main/java/com/gng25001/suivitdemedicaments/Menu.java
+++ SuivitdeMedicaments/app/src/main/java/com/gng25001/suivitdemedicaments/Menu.java
@@ -1,15 +1,41 @@
 package com.gng25001.suivitdemedicaments;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Menu extends AppCompatActivity {
 
+    //Elements of the layout
+    private Button btnMedList;
+    private Button btnSettings;
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
+        //Set the layout as the menu
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu);
+
+        getLayoutElements();
     }
 
+    /**
+     * Get the elements from the layout
+     */
+    private void getLayoutElements(){
+        btnMedList = findViewById(R.id.btnMenuGoToMedList);
+        btnSettings = findViewById(R.id.btnMenuSettings);
+    }
+
+
+    /**
+     * Function called by every button on the menu activity
+     * @param view the item that was clicked
+     */
+    public void onClick(View view){
+
+        
+    }
 }
