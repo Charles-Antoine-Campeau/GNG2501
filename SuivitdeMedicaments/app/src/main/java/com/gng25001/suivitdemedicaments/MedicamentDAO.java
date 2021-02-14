53afb113a1cdc167758d8336c33a102b07c8e200
Charles-Antoine-Campeau <campeauca@gmail.com>
2021-02-14 12:38:17 -0500

    Adding basic functions to MedicamentDAO

--- SuivitdeMedicaments/app/src/main/java/com/gng25001/suivitdemedicaments/MedicamentDAO.java
+++ SuivitdeMedicaments/app/src/main/java/com/gng25001/suivitdemedicaments/MedicamentDAO.java
@@ -1,4 +1,28 @@
 package com.gng25001.suivitdemedicaments;
 
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+@Dao
 public interface MedicamentDAO {
+
+    @Insert
+    public void insertNewMedicament(Medicament medicament);
+
+    @Update
+    public void updateMedicament(Medicament medicament);
+
+    @Delete
+    public void deleteMedicament(Medicament medicament);
+
+    @Query("SELECT * FROM Medicament")
+    public Medicament[] loadAllMedicament();
+
+    @Query("SELECT names FROM Medicament")
+    public List<Medicament> loadAllNames();
 }
