882d2cd7e771e35e336c7252d31b2869ec78a4a0
Charles-Antoine-Campeau <campeauca@gmail.com>
2021-02-13 21:34:19 -0500

    Adding data entity to Medicament class

--- SuivitdeMedicaments/app/src/main/java/com/gng25001/suivitdemedicaments/Medicament.java
+++ SuivitdeMedicaments/app/src/main/java/com/gng25001/suivitdemedicaments/Medicament.java
@@ -1,4 +1,34 @@
 package com.gng25001.suivitdemedicaments;
 
+import android.graphics.Bitmap;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity
 public class Medicament {
+
+    //Variable defining a medicament
+    @ColumnInfo(name = "images")
+    private Bitmap image;
+
+    @PrimaryKey
+    private String name;
+
+    @ColumnInfo(name = "doses")
+    private short dose;
+
+    @ColumnInfo(name = "waiting_time")
+    private short waitingTimeBeforeNewStockDays;
+
+    @ColumnInfo(name = "prescription_size")
+    private short prescriptionSize;
+
+    @ColumnInfo(name = "total_of_medicament")
+    private int totalOfMedicament;
+
+    public Medicament(){
+
+    }
 }
