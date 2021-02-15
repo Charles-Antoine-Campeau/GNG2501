e9367ca9065b480bf3bb40cea1b0ad32d78264f9
Charles-Antoine-Campeau <campeauca@gmail.com>
2021-02-14 23:14:12 -0500

    Adding basic functions to AppDatabase

--- SuivitdeMedicaments/app/src/main/java/com/gng25001/suivitdemedicaments/AppDatabase.java
+++ SuivitdeMedicaments/app/src/main/java/com/gng25001/suivitdemedicaments/AppDatabase.java
@@ -1,4 +1,48 @@
 package com.gng25001.suivitdemedicaments;
 
-public class AppDatabase {
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.room.Database;
+import androidx.room.DatabaseConfiguration;
+import androidx.room.InvalidationTracker;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteOpenHelper;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@Database(entities = {Medicament.class}, version = 1)
+public abstract class AppDatabase extends RoomDatabase {
+
+    /**
+     * Expose the MedicamentDAO
+     * @return an instance of MedicamentDAO
+     */
+    public abstract MedicamentDAO medicamentDAO();
+
+    //Instance for singleton
+    private static volatile AppDatabase INSTANCE;
+
+    //Thread pool to execute asynchronous background opperations
+    private static final int NUMBER_OF_THREADS = 4;
+    static final ExecutorService databaseWriteExecutor = Executors.newFixedThreadPool(NUMBER_OF_THREADS);
+
+    /**
+     * get the singleton instance of AppDatabase
+     * @param context
+     * @return the instance
+     */
+    static AppDatabase getDatabase(final Context context) {
+        if (INSTANCE == null) {
+            synchronized (AppDatabase.class) {
+                if(INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(), AppDatabase.class, "medicament_database").build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+
 }
