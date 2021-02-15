e15dd38fea3921f0132c6f9dd5bf595655525201
Charles-Antoine-Campeau <campeauca@gmail.com>
2021-02-15 14:18:47 -0500

    Adding basic functions to MedicamentViewModel

--- SuivitdeMedicaments/app/src/main/java/com/gng25001/suivitdemedicaments/MedicamentViewModel.java
+++ SuivitdeMedicaments/app/src/main/java/com/gng25001/suivitdemedicaments/MedicamentViewModel.java
@@ -1,4 +1,25 @@
 package com.gng25001.suivitdemedicaments;
 
-public class MedicamentViewModel {
+import android.app.Application;
+
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
+public class MedicamentViewModel extends AndroidViewModel {
+
+    private MedicamentRepository medicamentRepository;
+
+    private final LiveData<List<Medicament>> allMedicament;
+
+    public MedicamentViewModel (Application application) {
+        super(application);
+        medicamentRepository = new MedicamentRepository(application);
+        allMedicament = medicamentRepository.getAllMedicaments();
+    }
+
+    LiveData<List<Medicament>> getAllMedicament() {return allMedicament;}
+
+    public void insert(Medicament medicament) { medicamentRepository.insert(medicament);}
 }
