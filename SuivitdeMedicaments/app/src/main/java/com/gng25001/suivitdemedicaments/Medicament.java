2b281694df9d67058e228b32cab7b92c9e2d63eb
Charles-Antoine-Campeau <campeauca@gmail.com>
2021-02-14 23:31:33 -0500

    Adding constructors and getters to Medicament

--- SuivitdeMedicaments/app/src/main/java/com/gng25001/suivitdemedicaments/Medicament.java
+++ SuivitdeMedicaments/app/src/main/java/com/gng25001/suivitdemedicaments/Medicament.java
@@ -9,12 +9,14 @@ import androidx.room.PrimaryKey;
 @Entity
 public class Medicament {
 
-    //Variable defining a medicament
+    //**********************************************************************************************
+    //VARIABLES
+    @PrimaryKey
+    @ColumnInfo(name = "names")
+    private String name;
+
     @ColumnInfo(name = "images")
     private Bitmap image;
-
-    @PrimaryKey
-    private String name;
 
     @ColumnInfo(name = "doses")
     private short dose;
@@ -27,8 +29,63 @@ public class Medicament {
 
     @ColumnInfo(name = "total_of_medicament")
     private int totalOfMedicament;
+    //END OF VARIABLES
+    //**********************************************************************************************
 
-    public Medicament(){
+    //**********************************************************************************************
+    //CONSTRUCTORS
+    /**
+     * Complete constructor for Medicament
+     * @param name name of medicament
+     * @param image image of medicament
+     * @param dose preset quantity to be taken
+     * @param waitingTimeBeforeNewStockDays necessary time before new order to pharmacy
+     * @param prescriptionSize given quantity by pharmacy
+     * @param totalOfMedicament current total of medicament
+     */
+    public Medicament(String name, Bitmap image, short dose, short waitingTimeBeforeNewStockDays,
+                      short prescriptionSize, int totalOfMedicament) {
+        this.name = name;
+        this.image = image;
+        this.dose = dose;
+        this.waitingTimeBeforeNewStockDays = waitingTimeBeforeNewStockDays;
+        this.prescriptionSize = prescriptionSize;
+        this.totalOfMedicament = totalOfMedicament;
 
     }
+
+    /**
+     * Constructor without image
+     * @param name name of medicament
+     * @param dose preset quantity to be taken
+     * @param waitingTimeBeforeNewStockDays necessary time before new order to pharmacy
+     * @param prescriptionSize given quantity by pharmacy
+     * @param totalOfMedicament current total of medicament
+     */
+    public Medicament(String name, short dose, short waitingTimeBeforeNewStockDays,
+                      short prescriptionSize, int totalOfMedicament) {
+        this.name = name;
+        this.dose = dose;
+        this.waitingTimeBeforeNewStockDays = waitingTimeBeforeNewStockDays;
+        this.prescriptionSize = prescriptionSize;
+        this.totalOfMedicament = totalOfMedicament;
+    }
+    //END OF CONSTRUCTORS
+    //**********************************************************************************************
+
+    //**********************************************************************************************
+    //GETTERS
+    public String getName() {return name;}
+    public Bitmap getImage() {return image;}
+    public short getDose() {return dose;}
+    public short getWaitingTimeBeforeNewStockDays() {return waitingTimeBeforeNewStockDays;}
+    public short getPrescriptionSize() {return prescriptionSize;}
+    public int getTotalOfMedicament() {return totalOfMedicament;}
+    //END OF GETTERS
+    //**********************************************************************************************
+
+    //**********************************************************************************************
+    //SETTERS
+
+
 }
