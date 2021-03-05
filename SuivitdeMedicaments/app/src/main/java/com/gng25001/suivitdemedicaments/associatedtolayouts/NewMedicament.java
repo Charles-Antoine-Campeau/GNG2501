1ad32c0e623b55df71f32ccfc36ba0017de6c71c
campeauca@gmail.com <campeauca@gmail.com>
2021-03-05 11:28:22 -0500

    Adding onCreate method in NewMedicament class and adding class to the manifest

--- SuivitdeMedicaments/app/src/main/java/com/gng25001/suivitdemedicaments/associatedtolayouts/NewMedicament.java
+++ SuivitdeMedicaments/app/src/main/java/com/gng25001/suivitdemedicaments/associatedtolayouts/NewMedicament.java
@@ -1,6 +1,26 @@
 package com.gng25001.suivitdemedicaments.associatedtolayouts;
 
+import android.os.Bundle;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.gng25001.suivitdemedicaments.R;
+
 public class NewMedicament extends AppCompatActivity {
+
+    //**********************************************************************************************
+    //ONCREATE
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //set view as associated layout
+        setContentView(R.layout.new_medicament);
+    }
+
+    public void onClick(View view) {
+
+    }
 }
