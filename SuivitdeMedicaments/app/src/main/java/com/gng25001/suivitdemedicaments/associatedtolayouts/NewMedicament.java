f6b5616864b6f8f327af419d1b8d89deec6dcf0a
campeauca@gmail.com <campeauca@gmail.com>
2021-03-05 18:16:10 -0500

    Implement onCreate and part of onClick in NewMedicament, minor changes in new medicament layout and in strings values

--- SuivitdeMedicaments/app/src/main/java/com/gng25001/suivitdemedicaments/associatedtolayouts/NewMedicament.java
+++ SuivitdeMedicaments/app/src/main/java/com/gng25001/suivitdemedicaments/associatedtolayouts/NewMedicament.java
@@ -2,6 +2,7 @@ package com.gng25001.suivitdemedicaments.associatedtolayouts;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,17 +11,33 @@ import com.gng25001.suivitdemedicaments.R;
 public class NewMedicament extends AppCompatActivity {
 
     //**********************************************************************************************
-    //ONCREATE
+    //VARIABLES
+    private Button btnCancel;
+    private Button btnSave;
+    //END OF VARIABLES
+    //**********************************************************************************************
 
+    //**********************************************************************************************
+    //ONCREATE
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         //set view as associated layout
         setContentView(R.layout.new_medicament);
+
+        //get the cancel and save buttons
+        btnCancel = findViewById(R.id.btnCancelNewMed);
+        btnSave = findViewById(R.id.btnSaveNewMed);
     }
+    //END OF ONCREATE
+    //**********************************************************************************************
 
     public void onClick(View view) {
-
+        //determine which button was pressed
+        if (view == btnCancel) {
+            //discard all changes and goes back to the medication list
+            finish();
+        }
     }
 }
