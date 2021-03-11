8eb4c3e3bbae37e122b4c6df3bb44b1b8e5d4839
Charles-Antoine-Campeau <campeauca@gmail.com>
2021-03-10 22:41:48 -0500

    Creating a new activity to show when full screen notifications are displayed

---
+++ SuivitdeMedicaments/app/src/main/java/com/gng25001/suivitdemedicaments/associatedtolayouts/NotificationMessage.java
@@ -0,0 +1,16 @@
+package com.gng25001.suivitdemedicaments.associatedtolayouts;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.gng25001.suivitdemedicaments.R;
+
+public class NotificationMessage extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedBundleState) {
+        super.onCreate(savedBundleState);
+        setContentView(R.layout.notification_message);
+    }
+}
