acc8519557c19eb4daa36ccd9df79adccee5c6f5
Charles-Antoine-Campeau <campeauca@gmail.com>
2021-03-10 20:40:52 -0500

    Addig getInstance and constructor to CreateNotification

--- SuivitdeMedicaments/app/src/main/java/com/gng25001/suivitdemedicaments/CreateNotification.java
+++ SuivitdeMedicaments/app/src/main/java/com/gng25001/suivitdemedicaments/CreateNotification.java
@@ -1,4 +1,43 @@
-package com.gng25001.suivitdemedicaments.associatedtolayouts;
+package com.gng25001.suivitdemedicaments;
+
+import androidx.core.app.NotificationCompat;
 
 public class CreateNotification {
+
+    //**********************************************************************************************
+    //VARIABLES
+    private static CreateNotification instance;
+    //END OF VARIABLES
+    //**********************************************************************************************
+
+    //**********************************************************************************************
+    //CONSTRUCTOR
+
+    /**
+     * Constructor of the class
+     */
+    private CreateNotification() {
+        instance = this;
+    }
+    //END OF CONSTRUCTOR
+    //**********************************************************************************************
+
+    //**********************************************************************************************
+    //PUBLIC METHODS
+    /**
+     * Get the singleton instance of the class
+     * @return the instance of the class
+     */
+    public CreateNotification getInstance() {
+        if (instance == null) {
+            new CreateNotification();
+        }
+        return instance;
+    }
+    //END OF PUBLIC METHODS
+    //**********************************************************************************************
+
+    //**********************************************************************************************
+    //PRIVATE METHODS
+    
 }
