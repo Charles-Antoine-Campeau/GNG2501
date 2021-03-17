24bd86f7eec6af83de4831f0090bb617ab2b682c
Charles-Antoine-Campeau <campeauca@gmail.com>
2021-03-16 23:33:35 -0400

    Implementing basic opperations for the AlarmReceiver

--- SuivitdeMedicaments/app/src/main/java/com/gng25001/suivitdemedicaments/AlarmReceiver.java
+++ SuivitdeMedicaments/app/src/main/java/com/gng25001/suivitdemedicaments/AlarmReceiver.java
@@ -1,15 +1,43 @@
 package com.gng25001.suivitdemedicaments;
 
+import android.app.Notification;
+import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
+import com.gng25001.suivitdemedicaments.CreateNotification;
+
 public class AlarmReceiver extends BroadcastReceiver {
+
+    private NotificationManager mNotificationManager;
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        // TODO: This method is called when the BroadcastReceiver is receiving
-        // an Intent broadcast.
-        throw new UnsupportedOperationException("Not yet implemented");
+        mNotificationManager = (NotificationManager)
+                context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        deliverNotification(context);
+    }
+
+    private void deliverNotification (Context context) {
+
+        CreateNotification createNotification  = CreateNotification.getInstance();
+
+
+        try {
+            Notification notification = createNotification.buildNotification(
+                    context, "MEDICATION_TO_TAKE", "Title", "Text",
+                    Notification.CATEGORY_ALARM, true);
+
+            mNotificationManager.notify(1, notification);
+        }
+        catch (Exception e) {
+            //TO DO
+        }
+
+
+
+
     }
 }
