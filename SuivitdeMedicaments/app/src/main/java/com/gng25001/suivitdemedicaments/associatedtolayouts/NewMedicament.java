cef39eb169631f8676ff52c0dd68787539ac2482
Charles-Antoine-Campeau <campeauca@gmail.com>
2021-03-25 17:30:48 -0400

    Determining if all text box have a value

--- SuivitdeMedicaments/app/src/main/java/com/gng25001/suivitdemedicaments/associatedtolayouts/NewMedicament.java
+++ SuivitdeMedicaments/app/src/main/java/com/gng25001/suivitdemedicaments/associatedtolayouts/NewMedicament.java
@@ -1,12 +1,16 @@
 package com.gng25001.suivitdemedicaments.associatedtolayouts;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.gng25001.suivitdemedicaments.R;
+
+import org.w3c.dom.Text;
 
 public class NewMedicament extends AppCompatActivity {
 
@@ -14,6 +18,11 @@ public class NewMedicament extends AppCompatActivity {
     //VARIABLES
     private Button btnCancel;
     private Button btnSave;
+    private EditText etxtName;
+    private EditText etxtnDoses;
+    private EditText etxtnWaitingTimeNewDoses;
+    private EditText etxtnQuantityOfDosesInPrescriptions;
+    private EditText etxtnCurrentTotal;
     //END OF VARIABLES
     //**********************************************************************************************
 
@@ -29,6 +38,13 @@ public class NewMedicament extends AppCompatActivity {
         //get the cancel and save buttons
         btnCancel = findViewById(R.id.btnCancelNewMed);
         btnSave = findViewById(R.id.btnSaveNewMed);
+
+        //get the edit text items
+        etxtName = findViewById(R.id.etxtName);
+        etxtnDoses = findViewById(R.id.etxtnDoses);
+        etxtnWaitingTimeNewDoses = findViewById(R.id.etxtnWaitingTimeNewDoses);
+        etxtnQuantityOfDosesInPrescriptions = findViewById(R.id.etxtnQuantityOfDosesInPrescriptions);
+        etxtnCurrentTotal = findViewById(R.id.etxtnCurrentTotal);
     }
     //END OF ONCREATE
     //**********************************************************************************************
@@ -38,6 +54,30 @@ public class NewMedicament extends AppCompatActivity {
         if (view == btnCancel) {
             //discard all changes and goes back to the medication list
             finish();
+        }
+        if (view == btnSave) {
+            boolean hasError = false;
+            //make sure all edit texts have a value
+            if (TextUtils.isEmpty(etxtName.getText().toString())) {
+                etxtName.setError("A name is required");
+                hasError = true;
+            }
+            if (TextUtils.isEmpty(etxtnDoses.getText().toString())) {
+                etxtnDoses.setError("A dose is required");
+                hasError = true;
+            }
+            if (TextUtils.isEmpty(etxtnWaitingTimeNewDoses.getText().toString())) {
+                etxtnWaitingTimeNewDoses.setError("A waiting time si required");
+                hasError = true;
+            }
+            if (TextUtils.isEmpty(etxtnQuantityOfDosesInPrescriptions.getText().toString())) {
+                etxtnQuantityOfDosesInPrescriptions.setError("A quantity in each prescriptions is required");
+                hasError = true;
+            }
+            if (TextUtils.isEmpty(etxtnCurrentTotal.getText().toString())) {
+                etxtnCurrentTotal.setError("A current total is required");
+                hasError = true;
+            }
         }
     }
 }
