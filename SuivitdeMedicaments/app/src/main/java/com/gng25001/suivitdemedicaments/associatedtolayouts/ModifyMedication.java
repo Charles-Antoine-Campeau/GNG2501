b509c65ece7c63742e848022bc9e30fbee2b3739
Charles-Antoine-Campeau <campeauca@gmail.com>
2021-03-27 16:27:56 -0400

    Creating new layout to modify medication

---
+++ SuivitdeMedicaments/app/src/main/java/com/gng25001/suivitdemedicaments/associatedtolayouts/ModifyMedication.java
@@ -0,0 +1,13 @@
+package com.gng25001.suivitdemedicaments.associatedtolayouts;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ModifyMedication extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
