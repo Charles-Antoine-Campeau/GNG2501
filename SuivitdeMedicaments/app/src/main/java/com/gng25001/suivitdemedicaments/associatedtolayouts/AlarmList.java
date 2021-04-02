0de7f4ea1ea13f9d4b15f9a7374a3945da2da015
Charles-Antoine-Campeau <jcamp179@uottawa.ca>
2021-04-02 15:08:20 -0400

    Adding AlarmList to manifest

--- SuivitdeMedicaments/app/src/main/java/com/gng25001/suivitdemedicaments/associatedtolayouts/AlarmList.java
+++ SuivitdeMedicaments/app/src/main/java/com/gng25001/suivitdemedicaments/associatedtolayouts/AlarmList.java
@@ -1,4 +1,13 @@
 package com.gng25001.suivitdemedicaments.associatedtolayouts;
 
-public class AlarmList {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AlarmList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
