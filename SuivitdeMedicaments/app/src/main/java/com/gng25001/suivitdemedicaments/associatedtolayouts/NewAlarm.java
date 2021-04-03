99710df1723d005c5e6c440c8140aef82161a976
Charles-Antoine-Campeau <jcamp179@uottawa.ca>
2021-04-02 22:37:12 -0400

    Creating new alarm layout and class

---
+++ SuivitdeMedicaments/app/src/main/java/com/gng25001/suivitdemedicaments/associatedtolayouts/NewAlarm.java
@@ -0,0 +1,16 @@
+package com.gng25001.suivitdemedicaments.associatedtolayouts;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.gng25001.suivitdemedicaments.R;
+
+public class NewAlarm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.new_alarm);
+    }
+}
