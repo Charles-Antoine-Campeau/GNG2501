a6377605bc22ff2d3dbe6a70a33b8e7d7eef439a
Charles-Antoine-Campeau <jcamp179@uottawa.ca>
2021-04-04 17:25:58 -0400

    Completing the Alarm Receiver

--- SuivitdeMedicaments/app/src/main/java/com/gng25001/suivitdemedicaments/AlarmReceiver.java
+++ SuivitdeMedicaments/app/src/main/java/com/gng25001/suivitdemedicaments/AlarmReceiver.java
@@ -1,14 +1,28 @@
 package com.gng25001.suivitdemedicaments;
 
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
+import androidx.core.app.NotificationCompat;
+
+import com.gng25001.suivitdemedicaments.associatedtolayouts.NotificationMessage;
+
 public class AlarmReceiver extends BroadcastReceiver {
 
+    //**********************************************************************************************
+    //VARIABLES
     private NotificationManager mNotificationManager;
 
+    private final String MEDICATION_TO_TAKE = "MEDICATION_TO_TAKE";
+    private final String TO_DO_LATER = "TO_DO_LATER";
+    //END OF VARIABLES
+    //**********************************************************************************************
+
+    //**********************************************************************************************
+    //ONRECEIVE
     /**
      * Called when the BroadcastReceiver receives an Intent broadcast
      * @param context context in which the receiver is running
@@ -16,13 +30,47 @@ public class AlarmReceiver extends BroadcastReceiver {
      */
     @Override
     public void onReceive(Context context, Intent intent) {
-        mNotificationManager = (NotificationManager)
-                context.getSystemService(Context.NOTIFICATION_SERVICE);
+        mNotificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
 
-        deliverNotification(context);
+        deliverNotification (
+                context,
+                intent.getIntExtra("notificationID", 0),
+                intent.getStringExtra("medicationName")
+        );
     }
+    //END OF ONRECEIVE
+    //**********************************************************************************************
 
-    private void deliverNotification (Context context) {
-            //TODO
+    //**********************************************************************************************
+    //PRIVATE METHODS
+    /**
+     * Deliver the notification associated to the alarm
+     * @param context context in which the receiver is running
+     * @param notificationID id associated to the alarm and notification
+     * @param medicationName the medication for which the alarm is running
+     */
+    private void deliverNotification (Context context, int notificationID, String medicationName) {
+        //Intent for the layout when the notification is clicked
+        Intent contentIntent = new Intent(context, NotificationMessage.class);
+        contentIntent.putExtra("medicationName", medicationName);
+
+        PendingIntent contentPendingIntent = PendingIntent.getActivity(
+                context, notificationID, contentIntent, PendingIntent.FLAG_UPDATE_CURRENT
+        );
+
+        //Creates the notification
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, MEDICATION_TO_TAKE)
+                .setSmallIcon(R.drawable.notification_icon)
+                .setContentTitle("Medication to take")
+                .setContentText(medicationName)
+                .setContentIntent(contentPendingIntent)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setAutoCancel(true)
+                .setDefaults(NotificationCompat.DEFAULT_ALL);
+
+        //sends the notification
+        mNotificationManager.notify(notificationID, builder.build());
     }
+    //END OF PRIVATE METHODS
+    //**********************************************************************************************
 }
